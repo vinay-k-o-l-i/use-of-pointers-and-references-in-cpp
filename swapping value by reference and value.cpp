39d25c43c71d88fb43518475587fb392a27abec3
vinay-k-o-l-i <vinayramlukoli@gmail.com>
2025-08-27 20:16:27 +0530

    Create swapping value by reference and value.cpp

---
+++ swapping value by reference and value.cpp
@@ -0,0 +1,30 @@
+/* Vinay koli
+PRN 24070123133
+ENTC B2 */
+
+// swapping value by reference and value
+#include <iostream>
+using namespace std;
+
+void swap(int *a, int *b) 
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() 
+{
+    int a = 20;
+    int b = 50;
+
+    swap( &a, &b); 
+    cout <<"The value of a is " << a<<" "<<" and the value of b is "<< b << endl; 
+
+    return 0;
+}
+/* output for this code
+
+The value of a is 50  and the value of b is 20
+
+*/
